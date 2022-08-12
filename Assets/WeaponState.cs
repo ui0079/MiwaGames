30d44a2350f86f02ec4be6be80b9a9abebc3357d
ui0079 <hayashida@DESKTOP-QQN64I8.localdomain>
2022-08-12 22:27:13 +0900

    add weaponstatus

--- Assets/WeaponState.cs
+++ Assets/WeaponState.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 //武器の情報を保存するクラス
 public class WeaponState : MonoBehaviour
 {
-    public GameObject gameObject;
-    public ParticleSystem partcleSystem;
+    public GameObject gameObject; //ゲームオブジェクト
+    public ParticleSystem partcleSystem; //パーティクルシステム
+    public float speed; //発射速度
+    public float rate; //発射回数
+    public float damage; //攻撃力
 }
